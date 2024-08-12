d6fb683cd32f4c76856ca1e45891e3f2ea39888c
shikha thakur <shikhawebstep@gmail.com>
2024-08-12 18:23:09 +0530

    add css

--- src/Pages/UpdatePasswordForm.js
+++ src/Pages/UpdatePasswordForm.js
@@ -6,7 +6,7 @@ const UpdatePasswordForm = () => {
         newpass: '',
         c_newpass: '',
     });
-    const [passError,sePassError]=useState({});
+    const [passError, sePassError] = useState({});
     const handleChange = (event) => {
         const { name, value } = event.target;
         setNewPass((prev) => ({
@@ -15,14 +15,14 @@ const UpdatePasswordForm = () => {
 
     };
 
-    const validate=()=>{
-        const NewErr={};
-        if(!newPass.pass){NewErr.pass='This is Required'}
-        if(!newPass.newpass){NewErr.newpass='This is Required'}
-        if(!newPass.c_newpass){NewErr.c_newpass='This is Required'}
-        else if(newPass.c_newpass!==newPass.newpass){NewErr.c_newpass='Password does not match'}
+    const validate = () => {
+        const NewErr = {};
+        if (!newPass.pass) { NewErr.pass = 'This is Required' }
+        if (!newPass.newpass) { NewErr.newpass = 'This is Required' }
+        if (!newPass.c_newpass) { NewErr.c_newpass = 'This is Required' }
+        else if (newPass.c_newpass !== newPass.newpass) { NewErr.c_newpass = 'Password does not match' }
         return NewErr;
-  
+
     }
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -42,18 +42,18 @@ const UpdatePasswordForm = () => {
                 <div className="mb-6 text-left">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Current Password</label>
                     <input type="password" name="pass" className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="prevpassword" placeholder='********' onChange={handleChange} value={newPass.pass} />
-                          {passError.pass && <p className='text-red-500'>{passError.pass}</p>}
-                    </div>
+                    {passError.pass && <p className='text-red-500'>{passError.pass}</p>}
+                </div>
                 <div className="mb-6 text-left">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">New Password</label>
                     <input type="password" name="newpass" className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="newpassword" placeholder='********' onChange={handleChange} value={newPass.newPass} />
                     {passError.newpass && <p className='text-red-500'>{passError.newpass}</p>}
-                    </div>
+                </div>
                 <div className="mb-6 text-left">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Confirm New Password</label>
                     <input type="password" name="c_newpass" className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirmnewpassword" placeholder='********' onChange={handleChange} value={newPass.c_newpass} />
                     {passError.c_newpass && <p className='text-red-500'>{passError.c_newpass}</p>}
-                    </div>
+                </div>
                 <button type="submit" className='bg-green-400 text-white p-3 rounded-md w-full mb-4 hover:bg-green-200'>Update Password</button>
 
             </form>
